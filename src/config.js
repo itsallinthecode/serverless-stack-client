5d1fd33aa5da59cbb7cf71933f6f8975c6fa13ad
Rob <rob@itsallinthecode.com>
2020-02-10 16:46:09 +0000

    Building our react app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
-    s3: {
-      REGION: "eu-west-2",
-      BUCKET: "notes-app-test-01"
-    },
-    apiGateway: {
-      REGION: "eu-west-2",
-      URL: "https://kevvifyi0k.execute-api.eu-west-2.amazonaws.com/dev"
-    },
-    cognito: {
-      REGION: "eu-west-2",
-      USER_POOL_ID: "eu-west-2_SEGih9SEs",
-      APP_CLIENT_ID: "49sbgheq3tdh8qq7m3pee5ikat",
-      IDENTITY_POOL_ID: "eu-west-2:ace38e06-292b-40a1-be5d-a170e9f93a10"
-    }
-  };
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_og1bOvotlGKVmAylHC9NkQcD00ZQVD8Etq",
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "notes-app-test-01"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://kevvifyi0k.execute-api.eu-west-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_SEGih9SEs",
+    APP_CLIENT_ID: "49sbgheq3tdh8qq7m3pee5ikat",
+    IDENTITY_POOL_ID: "eu-west-2:ace38e06-292b-40a1-be5d-a170e9f93a10"
+  }
+};
